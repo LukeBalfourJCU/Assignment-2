53d6bbbb32e7217ada06dda3f9e880af1548d41b
LukeBalfourJCU <69950894+LukeBalfourJCU@users.noreply.github.com>
2020-10-05 21:19:29 +1000

    Update places.py

--- places.py
+++ places.py
@@ -14,23 +14,29 @@ Make method to determine importance ( if X <=2 )
 class Place:
     """..."""
     def __init__(self, name, country, priority, visited):
-        """Initialise a Car instance.
-
-        fuel: float, one unit of fuel drives one kilometre
-        """
         self.name = name
         self.country = country
         self.priority = 0
         self.visited = False
 
+    def mark_visited(self, visited, user_input):
+        user_input == str(input("Enter the name of the country to mark as visited: "))
+        if self.name == user_input:
+            self.visited == True
+
+
+    def unmark_visited(self, visited, user_input):
+        user_input == str(input("Enter the name of the country to mark as unvisited: "))
+        if self.name == user_input:
+            self.visited == False
+
+
+
     def is_important(self, priority, important):
-        if priority >= 2:
+        if priority <= 2:
             important = True
 
         return important
 
     def __str__(self):
         print("Priority: {} | {}, {}, {}").format(self.priority, self.name, self.country, self.visited)
-
-
-    pass
